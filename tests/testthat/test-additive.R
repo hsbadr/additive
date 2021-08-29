57daa4b10526548f37c495703f6bda19ee6f906d
Hamada S. Badr <hamada.s.badr@gmail.com>
2021-08-29 12:28:43 -0400

    additive: testthat: Use the 3rd edition

--- tests/testthat/test-additive.R
+++ tests/testthat/test-additive.R
@@ -67,7 +67,7 @@ test_that("additive execution", {
   expect_equal(coef(test_fit$fit), coef(gam_fit))
 
   # Check the predictions
-  expect_equal(test_pred$.pred, gam_pred, tolerance = 0.1)
+  expect_equal(test_pred$.pred, gam_pred, ignore_attr = TRUE)
 
   # Check the default engine
   expect_equal(additive()$engine, "mgcv")
